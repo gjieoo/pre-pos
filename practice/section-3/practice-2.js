f1ab310a3b531b2520d0b5c2604d053a24906855
gjieoo <15202979397@163.com>
2016-04-20 17:47:58 +0800

    commit section-3/practice-2.js

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,3 +1,26 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  for(var i=0;i<collection_a.length;i++)
+  {
+    if(isexit(collection_a[i].key,object_b.value))
+    {
+      if(collection_a[i].count / 3>0)
+      {
+        var num=Math.floor(collection_a[i].count / 3);
+        collection_a[i].count=collection_a[i].count -num;
+      }
+    }
+  }
+  return collection_a;
+
 }
+function isexit(element,collect)
+{
+  for(var i=0;i<collect.length;i++)
+  {
+    if(element===collect[i])
+      return true;
+  }
+  return false;
+}
+
