e6041ebc1df765076d935e6fcf349140104f0a60
gjieoo <15202979397@163.com>
2016-04-20 17:47:04 +0800

    commit section-2/practice-2.js

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,3 +1,24 @@
-function count_same_elements(collection) {
-  //在这里写入代码
+function count_same_elements(collection)
+{
+  var newArray=[{key:collection[0],count:1}];
+  var item=collection[0];
+  var newarr=[];
+  for(var i=1;i<collection.length;i++)
+  {
+    if(collection[i].length!=1)
+    {
+      newarr = collection[i].split("-");
+      newArray.push({key: newarr[0], count:Number( newarr[1])});
+    }
+    else if(item===collection[i])
+    {
+      newArray[newArray.length-1].count++;
+    }
+    else
+    {
+      item=collection[i];
+      newArray.push({key:collection[i],count:1});
+    }
+  }
+  return newArray;
 }
