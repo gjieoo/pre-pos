177af3a0db3bf9be6ff36cd11b82e23a551b6de6
gjieoo <15202979397@163.com>
2016-04-20 17:46:33 +0800

    commit section-2/practice-1.js

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,3 +1,19 @@
-function count_same_elements(collection) {
-  //在这里写入代码
+function count_same_elements(collection)
+{
+  var newArray=[{key:collection[0],count:1}];
+  var item=collection[0];
+  var newarr=[];
+  for(var i=1;i<collection.length;i++)
+  {
+    if(item===collection[i])
+    {
+      newArray[newArray.length-1].count++;
+    }
+    else
+    {
+      item=collection[i];
+      newArray.push({key:collection[i],count:1});
+    }
+  }
+  return newArray;
 }
