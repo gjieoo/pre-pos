970a426d60c34665e6c08b1bef89f9cfd48afc01
gjieoo <15202979397@163.com>
2016-04-20 17:44:42 +0800

    commit section-1/practice-2.js

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,3 +1,23 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var newArray=[];
+  for(var i=0;i<collection_a.length;i++) {
+    var item = collection_a[i];
+    if (isexit(item,collection_b[0]))
+    {
+      newArray.push(item);
+    }
+
+  }
+  return newArray;
+}
+function isexit(element,collection)
+{
+  for (var j = 0; j < collection.length; j++)
+  {
+    if(collection[j]===element) {
+      return true
+    }
+  }
+  return false;
 }
