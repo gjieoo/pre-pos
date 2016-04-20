2ebd1083d8cbdf000996c31a3bcf98873f2a9475
gjieoo <15202979397@163.com>
2016-04-20 17:47:45 +0800

    commit section-3/practice-1.js

--- practice/section-3/practice-1.js
+++ practice/section-3/practice-1.js
@@ -1,3 +1,21 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  for(var i=0;i<collection_a.length;i++)
+  {
+    if(isexit(collection_a[i].key,object_b.value))
+    {
+      collection_a[i].count--;
+    }
+  }
+  return collection_a;
+
+}
+function isexit(element,collect)
+{
+  for(var i=0;i<collect.length;i++)
+  {
+    if(element===collect[i])
+     return true;
+  }
+  return false;
 }
